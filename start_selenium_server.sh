0d8e8ac613a8dabd81d9ee87fe06df3cb4c563b5
Stefan Safranek <stefan@stefan>
2017-03-30 17:17:03 -0700

    updates

--- start_selenium_server.sh
+++ start_selenium_server.sh
@@ -9,11 +9,8 @@ java -jar selenium-server-standalone-3.3.1.jar -role node -hub http://localhost:
 # ./phantomjs --webdriver=8080 --webdriver-selenium-grid-hub=http://localhost:4444
 
 #sleep 5
+#java -jar selenium-server-standalone-3.3.1.jar
 #java -jar ios-server-0.6.5-jar-with-dependencies.jar \
 #    -hub http://localhost:4444/grid/register \
 #    -proxy org.uiautomation.ios.grid.IOSRemoteProxy \
 #    -port 3333 &
-
-
-#java -cp ios-grid-0.6.6-SNAPSHOT.jar:selenium-server-standalone.jar \
-#        org.openqa.grid.selenium.GridLauncher -role hub
